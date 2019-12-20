967ed104af2ac3da6655017df8d23d4766a7166c
jovmar34 <jovmar34@gmail.com>
2019-12-20 13:13:33 +0100

    Counter gg

--- docs/js/counter.js
+++ docs/js/counter.js
@@ -1,18 +1,26 @@
 var ctr = 0;
 const EUROPE_MEAL_INCR = 26.9;
 const AFRICA_MEAL_INCR = 1.66;
-const EUROPE_KCAL_INCR = 18831;
-const AFRICA_KCAL_INCR = 1159;
+
+var meal_europe, meal_africa, seconds;
+
+var od;
+
+function init() {
+    meal_europe = document.getElementById('meal_europe');
+    meal_africa = document.getElementById('meal_africa');
+    seconds = document.getElementById('seconds_ctr');
+}
+
+function zfill(string, size) {
+    return "0".repeat(size - string.length) + string;
+}
 
 function incr() {
     if (ctr > 60) return;    
-    var extra = "";
-    if (ctr < 10) extra = "0"
-    document.getElementById('seconds_ctr').innerHTML = extra + ctr.toString();
-    document.getElementById('meal_europe').innerHTML = Math.round(ctr*EUROPE_MEAL_INCR);
-    document.getElementById('kcal_europe').innerHTML = ctr*EUROPE_KCAL_INCR;
-    document.getElementById('meal_africa').innerHTML = Math.round(ctr*AFRICA_MEAL_INCR);
-    document.getElementById('kcal_africa').innerHTML = ctr*AFRICA_KCAL_INCR;
-    ctr++;
-    setTimeout(incr, 1000);
+    seconds.innerHTML = zfill(ctr.toString(),3)
+    meal_europe.innerHTML = zfill(Math.round(ctr*EUROPE_MEAL_INCR).toString(), 5);
+    meal_africa.innerHTML = zfill(Math.round(ctr*AFRICA_MEAL_INCR).toString(), 5);
+    ctr += 2;
+    setTimeout(incr, 2000);
 }
